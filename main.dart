fa99227bce68f6db63489660b1bf5b811a3b2436
kintanmn <102281479+kintanmn@users.noreply.github.com>
2023-09-13 21:20:44 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,5 @@
+import 'dart:core';
+
 class Mahasiswa {
   String nim;
   String nama;
@@ -6,10 +8,16 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    int tahunSekarang = DateTime.now().year;
+    int usia = tahunSekarang - tahunLahir;
+    print("Perkenalkan, nama saya $nama dengan NIM $nim dan usia saya $usia tahun.");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  Mahasiswa mhs1 = Mahasiswa("H1D020109", "Riana Dewi", 2002);
+  Mahasiswa mhs2 = Mahasiswa("H1D021125", "Lino Putra", 2003);
+
+  mhs1.perkenalan();
+  mhs2.perkenalan();
 }
